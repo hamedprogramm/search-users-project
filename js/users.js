408379fad1ace2db4f513c163a7fbf6cce07a7f4
hamed <81282529+hamedprogramm@users.noreply.github.com>
2022-04-19 22:11:42 +0430

    Update users.js

--- js/users.js
+++ js/users.js
@@ -3,36 +3,36 @@ export const usersData = [
         id: 1,
         imageURL : 'images/users2.jpg',
         userName: 'mary',
-        skill: 'front-end developer'
+        skill: 'python developer'
     },
     {
         id: 2,
         imageURL : 'images/users3.jpg',
         userName: 'robert',
-        skill: 'java developer'
+        skill: 'c++/c# developer'
     },
     {
         id: 3,
         imageURL : 'images/users4.jpg',
         userName: 'john',
-        skill: 'c++ developer'
+        skill: 'laravel developer'
     },
     {
         id: 4,
         imageURL : 'images/users5.jpg',
         userName: 'james',
-        skill: 'python developer'
+        skill: 'asp.net developer'
     },
     {
         id: 5,
         imageURL : 'images/users6.png',
         userName: 'peter',
-        skill: 'back-end developer'
+        skill: 'Django developer'
     },
     {
         id: 6,
         imageURL : 'images/users-profile.jpg',
         userName: 'joe',
-        skill: 'front-end developer'
+        skill: 'reactjs developer'
     },
 ]
